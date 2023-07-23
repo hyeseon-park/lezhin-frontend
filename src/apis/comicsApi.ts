e56cf7d1879b5c744152e8b8da236da525acf8de
hyeseon-park <lightning350@naver.com>
2023-07-24 04:24:06 +0900

    로그 삭제

--- src/apis/comicsApi.ts
+++ src/apis/comicsApi.ts
@@ -1,5 +1,4 @@
 export const getComicsList = async (genre: string, page: number) => {
-  console.log(genre, page); // 삭제
   const response = await fetch(`api/comics/${genre}?page=${page}`);
   return response;
 };
